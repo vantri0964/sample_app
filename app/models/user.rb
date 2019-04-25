df9f2d5b284113875f0bb72d28f0d0e5e8dfd812
vantri0964 <vantri27051998@gmail.com>
2019-04-25 21:43:38 +0700

    Chapter 8: Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true,
              length: {minimum: Settings.app.user.password_min_length}
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
+  test "valid signup information" do
     get signup_path
     assert_difference "User.count", 1 do
       post users_path, params: {user:
