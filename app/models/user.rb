87978adce2d06a55e8f00cb215c586af0d1dd7e2
vantri0964 <vantri27051998@gmail.com>
2019-04-25 21:43:38 +0700

    Chapter 6: Modeling users

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  before_save{self.email = email.downcase!}
+  validates :name, presence: true,
+             length: {maximum: Settings.app.user.name_max_length}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+             length: {maximum: Settings.app.user.email_max_length},
+                      format: {with: VALID_EMAIL_REGEX},
+                      uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true,
+             length: {minimum: Settings.app.user.password_min_length}
+end
